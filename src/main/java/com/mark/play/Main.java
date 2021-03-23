66155f22ce30c362e3acde6171ae4470ff335369
bjeong <bjeong@tibco.com>
2021-03-22 20:50:45 -0700

    table POC with a splitter

--- src/main/java/com/mark/play/Main.java
+++ src/main/java/com/mark/play/Main.java
@@ -6,6 +6,7 @@ import com.mark.play.actions.AppMenuBar;
 import com.mark.play.player.MyPlayer;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -59,13 +60,45 @@ public class Main implements IMain {
             }
         });
 
-        JPanel contentPane = new JPanel();
-        contentPane.setLayout(new BorderLayout());
+        JPanel playerContainer = new JPanel();
+        playerContainer.setLayout(new BorderLayout());
 
-        myPlayer = new MyPlayer(this, contentPane, resourceList);
+        String[] columnNames = {"First Name", "Last Name", "Sport", "# of Years", "Vegetarian"};
+
+        Object[][] data =
+        {
+                {"Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false)},
+                {"John", "Doe", "Rowing", new Integer(3), new Boolean(true)},
+                {"Sue", "Black", "Knitting", new Integer(2), new Boolean(false)},
+                {"Jane", "White", "Speed reading", new Integer(20), new Boolean(true)},
+                {"Joe", "Brown", "Pool", new Integer(10), new Boolean(false)}
+        };
+        JTable table = new JTable(data, columnNames);
+
+
+        TableColumn column = null;
+        for (int i = 0; i < 5; i++) {
+            column = table.getColumnModel().getColumn(i);
+            if (i == 2) {
+                column.setPreferredWidth(100); //third column is bigger
+                column.setMinWidth(100); //third column is bigger
+            } else {
+                column.setPreferredWidth(50);
+                column.setMinWidth(50); //third column is bigger
+            }
+        }
+
+        JScrollPane tableContainer = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, tableContainer, playerContainer);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(150);
+
+        myPlayer = new MyPlayer(this, playerContainer, resourceList);
 
         frame.setJMenuBar(new AppMenuBar(this));
-        frame.add(contentPane);
+        frame.add(splitPane);
         frame.pack();
         frame.setVisible(true);
 
@@ -102,7 +135,7 @@ public class Main implements IMain {
     }
 
     public void updateAppHeader() {
-        String header = String.format("%s (%s%s)", Utils.AppName,
+        String header = String.format("%s -%s%s", Utils.AppName,
            resourceList == null ? Utils.NoName : resourceList.getName(),
            resourceList == null ? "" : resourceList.isDirty() ? " *" : "");
         frame.setTitle(header);
