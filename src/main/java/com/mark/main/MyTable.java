da2f754aa7c78200dcd4ba7a4c662d8b454d822c
moonrise <bjeong@ymail.com>
2021-04-09 12:49:42 -0700

    resource table customer renderer (check point)

--- src/main/java/com/mark/main/MyTable.java
+++ src/main/java/com/mark/main/MyTable.java
@@ -6,40 +6,64 @@ import com.mark.resource.ResourceTableModel;
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
 
 public class MyTable extends JTable {
+    static class MyRenderer extends JLabel implements TableCellRenderer {
+        public MyRenderer() {
+            setHorizontalAlignment(SwingConstants.RIGHT);
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            setText(value.toString());
+            setToolTipText("tooltip here");
+            return this;
+        }
+    }
+
     private IMain main;
+    private ResourceTableModel tableModel;
+    private TableColumnModel columnModel;
 
     public MyTable(IMain main, ResourceList resourceList) {
         super(new ResourceTableModel(resourceList));
 
         this.main = main;
-        main.registerResourceListChangeListener(getMyModel());
+        this.tableModel = (ResourceTableModel) getModel();
+        this.columnModel = getColumnModel();
+
+        main.registerResourceListChangeListener(this.tableModel);
 
         setFillsViewportHeight(true);
         setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         setRowSelectionAllowed(true);
     }
 
-    private ResourceTableModel getMyModel() {
-        return (ResourceTableModel)getModel();
+    public void init() {
+        setRenderers();
     }
 
-    public TableCellRenderer getCellRenderer(int row, int column) {
-        return super.getCellRenderer(row, column);
+    public void setRenderers() {
+        MyTable.MyRenderer mr = new MyTable.MyRenderer();
+        columnModel.getColumn(4).setCellRenderer(mr);
+        columnModel.getColumn(5).setCellRenderer(mr);
     }
 
     public void setTableColumnWidths() {
+        init();
+
         //table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-        for (int i = 0; i < getModel().getColumnCount(); i++) {
-            TableColumn column = getColumnModel().getColumn(i);
-            int width = getMyModel().getColumnWidth(i);
+        for (int i = 0; i < tableModel.getColumnCount(); i++) {
+            TableColumn column = columnModel.getColumn(i);
+            int width = tableModel.getColumnWidth(i);
             column.setPreferredWidth(width);
             column.setMinWidth(width);
         }
     }
 
     public void updateResourceList(ResourceList resourceList) {
-        getMyModel().setResourceList(resourceList);
+        tableModel.setResourceList(resourceList);
     }
 }
