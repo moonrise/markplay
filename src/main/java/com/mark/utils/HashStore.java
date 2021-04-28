c02b4941a428a45e5bc64160ce41011867c95337
moonrise <bjeong@ymail.com>
2021-04-27 20:34:55 -0700

    MapDB POC

---
+++ src/main/java/com/mark/utils/HashStore.java
@@ -0,0 +1,56 @@
+package com.mark.utils;
+
+import com.mark.Log;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
+
+import java.util.concurrent.ConcurrentMap;
+
+public class HashStore {
+    public static HashStore Instance = new HashStore();
+
+    private final DB db;
+    private final ConcurrentMap map;
+
+    public static void main(String[] args) {
+        HashStore hashStore = HashStore.Instance;
+
+        //hashStore.put("h1", "v1");
+        //hashStore.put("h2", "v2");
+
+        Log.log("map get 1: %s", hashStore.get("h1"));
+        Log.log("map get 2: %s", hashStore.get("h2"));
+
+        hashStore.close();
+    }
+
+    private HashStore() {
+        this.db = DBMaker.fileDB("C:\\tmp\\markplay.db").fileMmapEnable().transactionEnable().make();
+        this.map = db.hashMap("map", Serializer.STRING, Serializer.STRING).createOrOpen();
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public void commit() {
+        db.commit();
+    }
+
+    public void put(String hash, String value) {
+        this.put(hash, value, true);
+    }
+
+    public void put(String hash, String value, boolean commit) {
+        map.put(hash, value);
+
+        if (commit) {
+            db.commit();
+        }
+    }
+
+    public String get(String hash) {
+        return (String)map.get(hash);
+    }
+}
